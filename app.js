3e8336184cdc2b80437b14236192f8f90e961502
KonstantinKurt <kurbatskykv@gmail.com>
2019-03-22 11:52:05 +0200

    2refactoring

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
 
 const redisConnection = require('./connections/redisConnection.js')
